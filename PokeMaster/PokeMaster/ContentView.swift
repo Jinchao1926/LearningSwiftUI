8be234827661f688ba4c36574d0d19dc6a02e1db
Edmodo-jinchao <jinchao@edmodo.com>
2021-08-17 14:27:00 +0800

    PokeMaster by SwiftUI

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by 林锦超 on 2021/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeMaster/PokeMaster/PokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterApp.swift
+//  PokeMaster
+//
+//  Created by 林锦超 on 2021/8/17.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
